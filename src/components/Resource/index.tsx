9d1c23840d636176140cabd32e2c845a7995260c
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2023-03-04 15:21:07 -0300

    feat(Resource): add subject field to override resource name permission

--- src/components/Resource/index.tsx
+++ src/components/Resource/index.tsx
@@ -1,11 +1,16 @@
 import React, { isValidElement } from 'react'
-import { ResourceContextProvider, ResourceProps } from 'react-admin'
+import { ResourceContextProvider, ResourceProps as RaResourceProps } from 'react-admin'
 import { Route, Routes } from 'react-router-dom'
 import { CASLAction } from '../../config'
 import { useCASL } from '../../contexts'
 import { createAbility, createRules } from '../../contexts/CASL/utils'
 
 import { InitPermissions } from '../InitPermissions'
+
+export interface ResourceProps extends RaResourceProps {
+  /** CASL permission subject to override resource name */
+  subject?: string
+}
 
 export const Resource = (props: ResourceProps) => {
   const { name } = props
@@ -18,23 +23,25 @@ export const Resource = (props: ResourceProps) => {
 }
 
 function CASLResource(props: ResourceProps) {
-  const { create: Create, edit: Edit, list: List, name, show: Show } = props
+  const { create: Create, edit: Edit, list: List, name, subject, show: Show } = props
   const { ability } = useCASL()
+
+  const permissionName = subject ? subject : name
 
   return (
     <>
       <InitPermissions />
       <Routes>
-        {Create && ability.can(CASLAction.Create, name) && (
+        {Create && ability.can(CASLAction.Create, permissionName) && (
           <Route path='create/*' element={isValidElement(Create) ? Create : <Create />} />
         )}
-        {Show && ability.can(CASLAction.Read, name) && (
+        {Show && ability.can(CASLAction.Read, permissionName) && (
           <Route path=':id/show/*' element={isValidElement(Show) ? Show : <Show />} />
         )}
-        {Edit && ability.can(CASLAction.Update, name) && (
+        {Edit && ability.can(CASLAction.Update, permissionName) && (
           <Route path=':id/*' element={isValidElement(Edit) ? Edit : <Edit />} />
         )}
-        {List && ability.can(CASLAction.Read, name) && (
+        {List && ability.can(CASLAction.Read, permissionName) && (
           <Route path='/*' element={isValidElement(List) ? List : <List />} />
         )}
         {props.children}
@@ -46,18 +53,32 @@ function CASLResource(props: ResourceProps) {
 Resource.raName = 'Resource'
 
 Resource.registerResource = (
-  { create, edit, icon, list, name, options, show, recordRepresentation }: ResourceProps,
+  {
+    create,
+    edit,
+    icon,
+    list,
+    name,
+    options,
+    show,
+    recordRepresentation,
+    hasCreate,
+    hasEdit,
+    hasShow,
+    subject,
+  }: ResourceProps,
   permissions: any,
 ) => {
   const rules = createRules(permissions)
   const ability = createAbility(rules)
+  const permissionName = subject ? subject : name
   return {
     name,
     options,
-    hasList: !!list && ability.can(CASLAction.Read, name),
-    hasCreate: !!create && ability.can(CASLAction.Create, name),
-    hasEdit: !!edit && ability.can(CASLAction.Update, name),
-    hasShow: !!show && ability.can(CASLAction.Read, name),
+    hasList: !!list && ability.can(CASLAction.Read, permissionName),
+    hasCreate: (!!create || !!hasCreate) && ability.can(CASLAction.Create, permissionName),
+    hasEdit: (!!edit || !!hasEdit) && ability.can(CASLAction.Update, permissionName),
+    hasShow: (!!show || !!hasShow) && ability.can(CASLAction.Read, permissionName),
     icon,
     recordRepresentation,
   }
