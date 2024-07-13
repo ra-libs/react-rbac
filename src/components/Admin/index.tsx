0d4895044c4634480d902556894b689a1f28ca26
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2024-07-13 18:46:12 -0300

    feat: :rocket: upgrade react-admin to v5
    BREAKING CHANGE: upgrade react-admin to v5

--- src/components/Admin/index.tsx
+++ src/components/Admin/index.tsx
@@ -3,6 +3,8 @@ import { Admin as RaAdmin, AdminProps } from 'react-admin'
 
 import { CASLProvider } from '../../contexts'
 
+// Overriding the ra-ui-materialui/src/AdminUI.tsx
+
 export function Admin(props: AdminProps) {
   const { children, ...rest } = props
   return (
--- src/components/actions/ListActions/index.tsx
+++ src/components/actions/ListActions/index.tsx
@@ -1,23 +1,53 @@
-import React from 'react'
-import { CreateButton, FilterButton, ListProps, TopToolbar, useResourceDefinition } from 'react-admin'
+import React, { cloneElement, useContext, useMemo } from 'react'
+import {
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  FilterContext,
+  ListActionsProps as RAListActionsProps,
+  sanitizeListRestProps,
+  TopToolbar,
+  useListContext,
+  useResourceContext,
+  useResourceDefinition,
+} from 'react-admin'
 
 import { CASLAction } from '../../../config'
 import { useCASL } from '../../../contexts'
 
-interface ListActionsProps {
-  filters?: Pick<ListProps, 'filters'>
-}
+interface ListActionsProps extends RAListActionsProps {}
+
+// Override ra-ui-materialui/src/list/ListActions.tsx
 
 export function ListActions(props: ListActionsProps) {
-  const { filters } = props
-  const { hasCreate, name } = useResourceDefinition()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { className, filters: filtersProp, hasCreate: _, ...rest } = props
+
+  const { displayedFilters, filterValues, exporter, showFilter, total } = useListContext()
+  const resource = useResourceContext(props)
+  const { hasCreate, name } = useResourceDefinition(props)
+  const filters = useContext(FilterContext) || filtersProp
   const { Can } = useCASL()
-  return (
-    <TopToolbar>
-      {filters && <FilterButton />}
-      <Can I={CASLAction.Create} a={name}>
-        {hasCreate && <CreateButton />}
-      </Can>
-    </TopToolbar>
+
+  return useMemo(
+    () => (
+      <TopToolbar className={className} {...sanitizeListRestProps(rest)}>
+        {filtersProp
+          ? cloneElement(filtersProp, {
+              resource,
+              showFilter,
+              displayedFilters,
+              filterValues,
+              context: 'button',
+            })
+          : filters && <FilterButton />}
+        <Can I={CASLAction.Create} a={name}>
+          {hasCreate && <CreateButton />}
+        </Can>
+        {exporter !== false && <ExportButton disabled={total === 0} resource={resource} />}
+      </TopToolbar>
+    ),
+    /* eslint-disable react-hooks/exhaustive-deps */
+    [resource, displayedFilters, filterValues, filtersProp, showFilter, filters, total, className, exporter, hasCreate],
   )
 }
